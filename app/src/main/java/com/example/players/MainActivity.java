1ce7df0832b80fc0a31943ef06aca2bdf4b2378d
moraesAN <moraes1945@gmail.com>
2021-03-22 21:10:11 -0300

    commit this

--- app/src/main/java/com/example/players/MainActivity.java
+++ app/src/main/java/com/example/players/MainActivity.java
@@ -10,13 +10,13 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton btnPlay, btnPause;
-    int controle = 0;
-    private MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.musica);
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.musica);
         mediaPlayer.start();
         btnPause = findViewById(R.id.btnPause);
         btnPlay = findViewById(R.id.btnPlay);
@@ -25,15 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void playPause(View v){
         if(mediaPlayer.isPlaying()){
-            btnPause.setVisibility(View.VISIBLE); // metodo para "Sumir" o botão
             btnPlay.setVisibility(View.GONE);
+            btnPause.setVisibility(View.VISIBLE); // método para "Aparecer" o botão
             mediaPlayer.pause();
-            controle = 1;
+
         } else {
-            mediaPlayer.start();
-            btnPlay.setVisibility(View.GONE); // metodo para "Sumir" o botão
+            btnPlay.setVisibility(View.GONE); // método para "Sumir" o botão
             btnPause.setVisibility(View.VISIBLE);
-            controle = 0;
+            mediaPlayer.start();
+
         }
 
     }
